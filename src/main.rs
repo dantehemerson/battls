c14377c2ccd65af145e390c3282a28e3fffb7628
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2026-01-16 17:22:55 -0500

    Simple print

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
 use anyhow::Result;
-use crossterm::{
-    terminal::{enable_raw_mode, disable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-    execute,
-};
-use std::{io, time::Duration};
 
 mod battery;
 mod ui;
@@ -11,24 +6,12 @@ mod ui;
 fn main() -> Result<()> {
     let batteries = battery::sysfs::read_batteries()?;
 
-    enable_raw_mode()?;
-
-    let mut stdout = io::stdout();
-
-    execute!(stdout, EnterAlternateScreen)?;
-
-    let backend = ratatui::backend::CrosstermBackend::new(stdout);
+    let backend = ratatui::backend::CrosstermBackend::new(std::io::stdout());
     let mut terminal = ratatui::Terminal::new(backend)?;
 
     terminal.draw(|f| {
         ui::render::render(f, &batteries);
     })?;
-
-    std::thread::sleep(Duration::from_millis(200));
-
-    disable_raw_mode()?;
-
-    execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
 
     Ok(())
 }
